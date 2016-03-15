6332d0f8cae29f1bddc68a0af739403ae1d8819a
Daniel Bölts <daniel@boelts.de>
2016-03-15 18:24:29 +0100

    Improved WebBrowser compatibility

--- KeeAnywhere/KeeAnywhereExt.cs
+++ KeeAnywhere/KeeAnywhereExt.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Linq;
 using System.Reflection;
 using System.Windows.Forms;
@@ -10,6 +11,7 @@ using KeePass.UI;
 using KeePassLib.Native;
 using KeePassLib.Serialization;
 using KeePassLib.Utility;
+using Microsoft.Win32;
 
 namespace KeeAnywhere
 {
@@ -51,6 +53,9 @@ namespace KeeAnywhere
 
             // Some binding redirection fixes for Google Drive API
             FixGoogleApiDependencyLoading();
+
+            // WebBrowser Feature Control 
+            SetBrowserFeatureControl();
 
             // Load the configuration
             _configService = new ConfigurationService(pluginHost);
@@ -98,6 +103,44 @@ namespace KeeAnywhere
 
             // Indicate that the plugin started successfully
             return true;
+        }
+
+        private void SetBrowserFeatureControl()
+        {
+            // http://msdn.microsoft.com/en-us/library/ee330720(v=vs.85).aspx
+
+            // FeatureControl settings are per-process
+            var fileName = System.IO.Path.GetFileName(Process.GetCurrentProcess().MainModule.FileName);
+
+            // make the control is not running inside Visual Studio Designer
+            if (string.Compare(fileName, "devenv.exe", true) == 0 || string.Compare(fileName, "XDesProc.exe", true) == 0)
+                return;
+
+            BrowserHelper.SetBrowserFeatureControlKey("FEATURE_BROWSER_EMULATION", fileName, BrowserHelper.GetBrowserEmulationMode()); // Webpages containing standards-based !DOCTYPE directives are displayed in IE10 Standards mode.
+            //BrowserHelper.SetBrowserFeatureControlKey("FEATURE_SCRIPTURL_MITIGATION", fileName, 1);
+            //SetBrowserFeatureControlKey("FEATURE_AJAX_CONNECTIONEVENTS", fileName, 1);
+            //SetBrowserFeatureControlKey("FEATURE_ENABLE_CLIPCHILDREN_OPTIMIZATION", fileName, 1);
+            //SetBrowserFeatureControlKey("FEATURE_MANAGE_SCRIPT_CIRCULAR_REFS", fileName, 1);
+            //SetBrowserFeatureControlKey("FEATURE_DOMSTORAGE ", fileName, 1);
+            //SetBrowserFeatureControlKey("FEATURE_GPU_RENDERING ", fileName, 1);
+            //SetBrowserFeatureControlKey("FEATURE_IVIEWOBJECTDRAW_DMLT9_WITH_GDI  ", fileName, 0);
+            //SetBrowserFeatureControlKey("FEATURE_DISABLE_LEGACY_COMPRESSION", fileName, 1);
+            //SetBrowserFeatureControlKey("FEATURE_LOCALMACHINE_LOCKDOWN", fileName, 0);
+            //SetBrowserFeatureControlKey("FEATURE_BLOCK_LMZ_OBJECT", fileName, 0);
+            //SetBrowserFeatureControlKey("FEATURE_BLOCK_LMZ_SCRIPT", fileName, 0);
+            //SetBrowserFeatureControlKey("FEATURE_DISABLE_NAVIGATION_SOUNDS", fileName, 1);
+            //SetBrowserFeatureControlKey("FEATURE_SCRIPTURL_MITIGATION", fileName, 1);
+            //SetBrowserFeatureControlKey("FEATURE_SPELLCHECKING", fileName, 0);
+            //SetBrowserFeatureControlKey("FEATURE_STATUS_BAR_THROTTLING", fileName, 1);
+            //SetBrowserFeatureControlKey("FEATURE_TABBED_BROWSING", fileName, 1);
+            //SetBrowserFeatureControlKey("FEATURE_VALIDATE_NAVIGATE_URL", fileName, 1);
+            //SetBrowserFeatureControlKey("FEATURE_WEBOC_DOCUMENT_ZOOM", fileName, 1);
+            //SetBrowserFeatureControlKey("FEATURE_WEBOC_POPUPMANAGEMENT", fileName, 0);
+            //SetBrowserFeatureControlKey("FEATURE_WEBOC_MOVESIZECHILD", fileName, 1);
+            //SetBrowserFeatureControlKey("FEATURE_ADDON_MANAGEMENT", fileName, 0);
+            //SetBrowserFeatureControlKey("FEATURE_WEBSOCKET", fileName, 1);
+            //SetBrowserFeatureControlKey("FEATURE_WINDOW_RESTRICTIONS ", fileName, 0);
+            //SetBrowserFeatureControlKey("FEATURE_XMLHTTP", fileName, 1);
         }
 
         private void OnSaveToCloudDrive(object sender, EventArgs e)
