2e1866d370b4708f4cc2bdf1f5060029320a6618
Daniel Bölts <daniel@boelts.de>
2016-09-01 15:05:11 +0200

    Starting 1.4.0

--- build.cmd
+++ build.cmd
@@ -1,5 +1,5 @@
 @echo off
-set version=1.3.0
+set version=1.4.0-unstable
 set zip="packages\7-Zip.CommandLine.9.20.0\tools\7za.exe"
 set msbuildcmd="C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\Tools\VsMSBuildCmd.bat"
 
