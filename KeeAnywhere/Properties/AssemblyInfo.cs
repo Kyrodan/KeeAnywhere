5939c15b746cd3814b568c1a899ff695e5f9f4c0
Daniel Bölts <daniel@boelts.de>
2018-09-20 16:27:25 +0200

    Preparing 1.5.0

--- KeeAnywhere/Properties/AssemblyInfo.cs
+++ KeeAnywhere/Properties/AssemblyInfo.cs
@@ -33,4 +33,4 @@ using System.Runtime.InteropServices;
 //[assembly: AssemblyVersion("0.1.0.0")]
 [assembly: AssemblyVersion("1.5.0.0")]
 [assembly: AssemblyFileVersion("1.5.0.0")]
-[assembly: AssemblyInformationalVersion("1.5.0-unstable")]
+[assembly: AssemblyInformationalVersion("1.5.0")]
