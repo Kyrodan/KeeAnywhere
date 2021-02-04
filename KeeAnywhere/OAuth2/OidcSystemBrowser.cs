9404b4a459dd3b5adf9de4b358d0592feeef5f3b
Daniel Bölts <daniel@boelts.de>
2021-02-04 11:39:06 +0100

    #89 Reimplement Account Authentication not using internal Web Browser

--- KeeAnywhere/OAuth2/OidcSystemBrowser.cs
+++ KeeAnywhere/OAuth2/OidcSystemBrowser.cs
@@ -199,24 +199,24 @@ namespace KeeAnywhere.OAuth2
             }
             catch
             {
-                // hack because of this: https://github.com/dotnet/corefx/issues/10361
-                if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
-                {
-                    url = url.Replace("&", "^&");
-                    Process.Start(new ProcessStartInfo("cmd", "/c start " + url) { CreateNoWindow = true });
-                }
-                else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
-                {
-                    Process.Start("xdg-open", url);
-                }
-                else if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
-                {
-                    Process.Start("open", url);
-                }
-                else
-                {
+                //// hack because of this: https://github.com/dotnet/corefx/issues/10361
+                //if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+                //{
+                //    url = url.Replace("&", "^&");
+                //    Process.Start(new ProcessStartInfo("cmd", "/c start " + url) { CreateNoWindow = true });
+                //}
+                //else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+                //{
+                //    Process.Start("xdg-open", url);
+                //}
+                //else if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+                //{
+                //    Process.Start("open", url);
+                //}
+                //else
+                //{
                     throw;
-                }
+                //}
             }
         }
     }
