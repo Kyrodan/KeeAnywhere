2d788485276d5af22e91b87b31fa4b8b53c646e2
Opalium <Opalium@opalium.net>
2021-05-24 16:32:37 +0300

    Fix dependencies issues (thanks @Kyrodan !)

--- KeeAnywhere/KeeAnywhereExt.cs
+++ KeeAnywhere/KeeAnywhereExt.cs
@@ -260,7 +260,10 @@ namespace KeeAnywhere
             var names = new[] {
                 "System.Net.Http.Primitives",
                 "Newtonsoft.Json",
-                "Microsoft.Graph.Core"
+                "Microsoft.Graph.Core",
+                "Google.Apis",
+                "Google.Apis.Auth",
+                "Google.Apis.Core"
             //    "System.Text.Encodings.Web",
             //    "System.Runtime.CompilerServices.Unsafe"
             };
